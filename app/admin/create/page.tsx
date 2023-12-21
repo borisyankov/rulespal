a2c5fe9f5169b5d3d7f1550ce47ef98679984dc8
Boris Yankov <borisyankov@gmail.com>
2023-12-21 02:51:13 +0200

    Add some basic admin functionality

---
+++ app/admin/create/page.tsx
@@ -0,0 +1,78 @@
+import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
+import { FormEvent } from "react";
+import xml2js from "xml2js";
+
+async function getGameInfo(gameId: number) {
+  try {
+    const response = await fetch(
+      `https://boardgamegeek.com/xmlapi2/thing?id=${gameId}`
+    );
+    const data = await response.text();
+    const parser = new xml2js.Parser();
+
+    parser
+      .parseStringPromise(data)
+      .then(function (result) {
+        console.log(JSON.stringify(result, null, 2));
+        // Process the result here
+      })
+      .catch(function (err) {
+        console.error("Error parsing XML:", err);
+      });
+  } catch (error) {
+    console.error("Error fetching game data:", error);
+  }
+}
+
+export default function Assistant() {
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        'use server'
+        // event.preventDefault()
+     
+        const formData = new FormData(event.currentTarget)
+        // const response = await fetch('/api/submit', {
+        //   method: 'POST',
+        //   body: formData,
+        // })
+
+        console.log(formData);
+
+        // getGameInfo(123456);
+     
+        // Handle response if necessary
+        // const data = await response.json()
+        // ...
+      }
+
+  return (
+    <main className="flex min-h-screen flex-col p-24">
+      <h1 className="text-5xl">Create assistant</h1>
+      <form onSubmit={onSubmit}>
+        <div className="pb-5">
+          <label
+            htmlFor="bggId"
+            className="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900"
+          >
+            Game BGG Id
+          </label>
+          <div className="mt-2">
+            <input
+              type="number"
+              name="bggId"
+              id="bggId"
+              className="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              required
+              placeholder="Jane Smith"
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Create
+        </button>
+      </form>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Inputer from "./inputer";
+import Themer from "./themer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Themer />
       <h1 className="text-5xl">Dune Imperium</h1>
       <h2>Ask any rules question. Get answers directly from the rule book.</h2>
 
