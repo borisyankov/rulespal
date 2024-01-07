6fba530d6eb1f0513e7054b5723ad91abbdcc71a
Boris Yankov <borisyankov@gmail.com>
2024-01-07 17:50:42 +0200

    Fix viewport configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from 'next';
-import { Montserrat } from 'next/font/google';
-import './globals.css'
+import type { Metadata, Viewport } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
 
-const montserratt = Montserrat({ subsets: ['latin'], weight: '400' })
+const montserratt = Montserrat({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
-  title: 'RulesPal',
-  description: 'Rules for board games, answered by your AI pal.',
-  viewport: {},
-  // viewport: {
-  //   width: 'device-width',
-  //   initialScale: 1,
-  //   interactiveWidget: 'resizes-content',
-  // },
-}
+  title: "RulesPal",
+  description: "Rules for board games, answered by your AI pal.",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  interactiveWidget: "resizes-content",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`h-full ${montserratt.className}`}>{children}</body>
     </html>
-  )
+  );
 }
