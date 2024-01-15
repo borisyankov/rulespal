fcac96c4403951ef1fcd669bca6a735be4f8866e
Boris Yankov <borisyankov@gmail.com>
2024-01-15 18:29:48 +0200

    Fix file upload function

--- app/admin/games/[id]/edit/file-upload.tsx
+++ app/admin/games/[id]/edit/file-upload.tsx
@@ -5,26 +5,26 @@ import { Button } from "@/app/ui/button";
 import { ArrowUpTrayIcon } from "@heroicons/react/24/outline";
 import { parseRulebook } from "@/app/lib/rag";
 
-export default async function FileUpload() {
-  function handleUpload(e: React.ChangeEvent<HTMLInputElement>) {
-    e.preventDefault(); 
+export default function FileUpload() {
+  function handleUpload() {
+    // e.preventDefault(); 
     parseRulebook();
     return;
-    const file = e.target.files[0];
-    if (!file) {
-      return;
-    }
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const contents = e.target.result;
-      console.log(contents);
-    };
-    reader.readAsText(file);
+    // const file = e.target.files[0];
+    // if (!file) {
+    //   return;
+    // }
+    // const reader = new FileReader();
+    // reader.onload = (e) => {
+    //   const contents = e.target.result;
+    //   console.log(contents);
+    // };
+    // reader.readAsText(file);
   }
   return (
     <form className="flex w-full max-w-sm gap-2 mt-8">
       <Input id="picture" type="file" />
-      <Button onClick={handleUpload}>
+      <Button onClick={(e) => { handleUpload(); }}>
         <ArrowUpTrayIcon className="mr-2 h-4 w-4" /> Upload
       </Button>
     </form>
--- app/rules/[id]/page.tsx
+++ app/rules/[id]/page.tsx
@@ -2,12 +2,13 @@
 
 import Markdown from "react-markdown";
 import rulebook from "/rulebooks/scout_rulebook.md";
+import remarkGfm from "remark-gfm";
 
 export default function Rules({ params }: { params: { id: string } }) {
   return (
     <div className="flex min-h-screen flex-col max-w-lg mx-auto">
-      <Markdown className="prose prose-zinc dark:prose-invert mb-10">
-        {rulebook}
+      <Markdown className="prose prose-zinc dark:prose-invert mb-10"  remarkPlugins={[remarkGfm]}>
+        {rulebook.toString()}
       </Markdown>
     </div>
   );
