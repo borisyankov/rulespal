67c6924483553628fc4da110ff1da94e5a180f91
Boris Yankov <borisyankov@gmail.com>
2024-01-21 18:24:01 +0200

    Configure to use Perplexity.AI

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,29 +1,34 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-import { searchFor } from '@/app/lib/data';
-import { getPrompt } from './prompt';
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { searchFor } from "@/app/lib/data";
+import { getPrompt } from "./prompt";
 
 const openai = new OpenAI({
-  baseURL: "https://oai.hconeai.com/v1",
+  apiKey: process.env.PERPLEXITY_API_KEY || '',
+  baseURL: "https://gateway.hconeai.com",
   defaultHeaders: {
     "Helicone-Auth": "Bearer sk-6f2u4iq-qqfeziq-ukhmmea-6ra4t5y",
+    "Helicone-Target-Url": "https://api.perplexity.ai",
+    "Helicone-Target-Provider": "Perplexity",
   },
 });
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   let { messages } = await req.json();
 
   const lastMessage = messages[messages.length - 1];
   const foundRules = await searchFor(lastMessage.content);
-  const rulesExcerpt = foundRules.map((x, i) => x.content + ` 【${i}†source】`).join('\n');
-  console.log(foundRules.map((x) => x.content).join('\n'));
+  const rulesExcerpt = foundRules
+    .map((x, i) => `${x.content} 【${i}†source】`)
+    .join("\n");
+  console.log(foundRules.map((x) => x.content).join("\n"));
   console.log(getPrompt(rulesExcerpt));
-  if (messages[0].role !== 'system') {
+  if (messages[0].role !== "system") {
     messages = [
       {
-        role: 'system',
+        role: "system",
         content: getPrompt(rulesExcerpt),
       },
       ...messages,
@@ -31,12 +36,11 @@ export async function POST(req: Request) {
   }
 
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: "pplx-70b-online",
+    // model: 'gpt-3.5-turbo',
     stream: true,
     temperature: 0,
     messages,
-    // frequency_penalty: 1,
-    // presence_penalty: 1,
   });
 
   // Convert the response into a friendly text-stream
