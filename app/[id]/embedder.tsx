ba56b882d127f79f4b70ab5be8e4f12e793c9c8a
Boris Yankov <borisyankov@gmail.com>
2024-02-04 02:35:15 +0200

    Update dependencies

--- app/[id]/embedder.tsx
+++
@@ -1,9 +0,0 @@
-import { docToEmbeddings } from '../lib/old-actions';
-import games from '@/data/games';
-
-export default async function Embedder() {
-  const gameRulebook = (await import(`../../data/rulebooks/${games[4].code}_rulebook.md`)).default;
-  const embeddings = await docToEmbeddings(gameRulebook);
-  console.log(embeddings);
-  return <div className="flex min-h-screen flex-col">DONE</div>;
-}
