93b7e6080abb769fae10bc11d0f907e2ea5a85c5
Boris Yankov <borisyankov@gmail.com>
2024-03-02 02:45:20 +0200

    Switch to GPT 4, regenerate embeddings for Anachrony

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: Request) {
   }
 
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4-turbo-preview',
     stream: true,
     temperature: 0,
     messages,
