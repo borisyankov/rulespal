0047d78967bea1dd0fd28b1d630faea4565cff43
Boris Yankov <borisyankov@gmail.com>
2024-03-03 22:58:07 +0200

    Rework themer to avoid hydration mismatch

--- app/ui/themer.tsx
+++ app/ui/themer.tsx
@@ -6,21 +6,14 @@ import { useTheme } from "next-themes";
 export default function Themer() {
   const { theme, setTheme } = useTheme();
 
-  // if (theme === undefined) return null;
-
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
-  const Icon = theme === "dark" ? SunIcon : MoonIcon;
-
   return (
     <div className="flex cursor-pointer flex-row">
-      <Icon
-        className="size-10 rounded-full bg-gray-500/10 p-2"
-        suppressHydrationWarning
-        onClick={toggleTheme}
-      />
+      <SunIcon className="size-10 rounded-full bg-gray-500/10 p-2 hidden dark:block hover:bg-gray-500/30" onClick={toggleTheme} />
+      <MoonIcon className="size-10 rounded-full bg-gray-500/10 p-2 is-light dark:hidden hover:bg-gray-500/30" onClick={toggleTheme} />
     </div>
   );
 }
