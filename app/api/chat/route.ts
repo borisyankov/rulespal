30ba351a3639dfb7f7fc06daa7b582d781186737
Boris Yankov <borisyankov@gmail.com>
2024-04-20 09:44:37 +0300

    Use LLAMA3 70b via Groq

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,9 +7,13 @@ import {
 import { searchFor } from '@/app/lib/actions';
 
 const openai = new OpenAI({
-  baseURL: "https://oai.hconeai.com/v1",
+  baseURL: "https://oai.hconeai.com/openai/v1",
   defaultHeaders: {
+    Authorization: "Bearer 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",
     "Helicone-Auth": "Bearer sk-6f2u4iq-qqfeziq-ukhmmea-6ra4t5y",
+    "Content-Type": "application/json",
+    "Helicone-Target-Provider": "Groq",
+    "Helicone-Target-Url": "https://api.groq.com",
   },
 });
 
@@ -41,7 +45,7 @@ export async function POST(req: Request) {
   }
 
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'llama3-70b-8192', // model: 'gpt-3.5-turbo',
     stream: true,
     temperature: 0,
     messages,
