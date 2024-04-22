4a80979d6f089fab6efaabf235aa6655bb036d57
Boris Yankov <borisyankov@gmail.com>
2024-04-23 00:18:19 +0300

    Add simpe footer

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function Header({ children }: Props) {
   return (
-    <header className="top-0 inset-x-0 py-1">
+    <header className="py-2">
       <div className="flex w-full m-auto px-2 max-w-screen-md flex-row items-center justify-between gap-8 text-white/80">
         {children}
       </div>
