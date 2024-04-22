36c4ac4bde48779c2068f714e043a46f5d9711af
Boris Yankov <borisyankov@gmail.com>
2024-04-22 23:57:17 +0300

    Redesign Citation look

--- app/ui/citation.tsx
+++ app/ui/citation.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Quote } from "lucide-react"
+import { BookOpenTextIcon, ArrowRightIcon } from "lucide-react";
 import type { Game } from "../lib/definitions";
 
 type Props = {
@@ -11,10 +11,15 @@ export default function Citation({ game, text }: Props) {
   if (!text || text.length === 0) {
     return null;
   }
+  const regex = /^#{1,6}\s*(.*)$/m;
+  const match = text.match(regex);
+  const matched = match && match.length === 2;
+  const href = matched ? match[1].toLowerCase().replaceAll(' ', '-') : '';
   return (
-    <Link href={`/${game.code}/rules`} title={text}>
-      <span className="inline-block bg-primary rounded-full align-middle hover:animate-pulse">
-        <Quote size={24} className="p-1.5	text-gray-50" />
+    <Link href={`/${game.code}/rulebook#${href}`} title={text} scroll>
+      <span className="flex p-1 w-16 bg-primary rounded-full align-middle hover:animate-pulse">
+        <ArrowRightIcon size={24} className="p-1 text-gray-50" />
+        <BookOpenTextIcon size={24} className="p-1	text-gray-50" /> 
       </span>
     </Link>
   );
