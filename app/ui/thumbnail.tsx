387c39cc01a58d1ae1827047b289f4d56a499847
Boris Yankov <borisyankov@gmail.com>
2026-08-02 02:17:28 +0300

    Lazy-load game thumbnails on main page
    Defer offscreen thumbnail image fetches until they approach the viewport instead of loading all eagerly.

--- app/ui/thumbnail.tsx
+++ app/ui/thumbnail.tsx
@@ -7,6 +7,6 @@ type Props = {
 
 export default function Thumbnail({ game, className = '' }: Props) {
   return (
-    <img className={`object-contain rounded-lg ${className}`} src={`/thumbs/${game.code}.jpg`} alt={`${game.name} thumbnail`} />
+    <img className={`object-contain rounded-lg ${className}`} src={`/thumbs/${game.code}.jpg`} alt={`${game.name} thumbnail`} loading="lazy" decoding="async" />
   );
 }
